65598395724b20ee7f5236f9fce785afecb1a56f
y-okudera <okudera_yuki@andfactory.co.jp>
2025-02-08 13:43:27 +0900

    Implement a search page.

---
+++ src/styles/theme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#0070f3",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+    background: {
+      default: "#f9f9f9",
+    },
+  },
+  typography: {
+    fontFamily: "'Arial', 'sans-serif'",
+  },
+});
